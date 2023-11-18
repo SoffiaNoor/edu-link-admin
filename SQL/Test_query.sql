1d42a2e99e4ffacd0f5f5c88e31557725162f1a5
SoffiaNoor <soffianaisyah0@gmail.com>
2023-11-18 11:47:06 +0700

    *delete sql

--- SQL/Test_query.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO MataKuliah(IDMK,NamaMK) VALUES('MK301','Sistem Basis Data')
-
-UPDATE MataKuliah SET SKS = 4, NamaMK = 'Teknologi Basis Data' WHERE IDMK = 'MK301'
-
-SELECT * FROM AuditMK
-SELECT * FROM MataKuliah
-
-DROP TABLE AuditMK
-DROP TABLE MataKuliah
--- SQL/user-02112023.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users(
-	ID INT NOT NULL IDENTITY(1,1),
-	username varchar(50),
-	password varchar(50),
-	PRIMARY KEY(ID)	
-)
-
-insert into users(username,password) values('tester1','tes123')
-select * from users
